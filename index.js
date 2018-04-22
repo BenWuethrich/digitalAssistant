320216d2e0f70a58f130d08dc29c3477b6f37ab3
BenWuethrich <36738195+BenWuethrich@users.noreply.github.com>
2018-04-22 13:08:35 +0200

    audio update

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ restService.post("/audio", function(req, res) {
       speech =
         '<speak><audio src="https://mgwalperswil.ch/wp-content/uploads/2018/04/079-kurzzz.mp3">did not get your audio file</audio></speak>';
       break;
+	case "was stimmt bi dir nööd":
+      speech =
+        '<speak><audio src="http://mgwalperswil.ch/wp-content/uploads/2018/04/Hey-kurzzz.mp3">did not get your audio file</audio></speak>';
+      break;
     case "music two":
       speech =
         '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
